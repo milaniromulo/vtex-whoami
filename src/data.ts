2ca65b6e7ee073c3d9e18dae7571d164abcb5145
milaniromulo <milaniromulo@gmail.com>
2023-08-12 21:06:37 -0300

    feat: add hasVTEXToolbelt method, to check if toolbelt is installed

--- src/data.ts
+++ src/data.ts
@@ -2,11 +2,14 @@ import * as fs from 'fs';
 import { join } from 'path';
 import { homedir } from 'os';
 
-export default function VTEXData() {
+export default function DataVTEX() {
     const VTEX_FOLDER = join(homedir(), '.vtex');
     const SESSION_FOLDER = join(VTEX_FOLDER, 'session');
     const SESSION = join(SESSION_FOLDER, 'session.json');
     const WORKSPACE = join(SESSION_FOLDER, 'workspace.json');
+
+    console.log(fs.existsSync(VTEX_FOLDER))
+    console.log(fs.existsSync(SESSION))
 
     const { account, login } = JSON.parse(fs.readFileSync(SESSION, 'utf8'));
     const { currentWorkspace } = JSON.parse(fs.readFileSync(WORKSPACE, 'utf8'));
@@ -17,4 +20,9 @@ export default function VTEXData() {
         currentWorkspace,
         WORKSPACE
     };
+}
+
+export function hasVTEXToolbelt() {
+    const VTEX_FOLDER = join(homedir(), '.vtex');
+    return fs.existsSync(VTEX_FOLDER)
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,36 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import VTEXData from './data';
+import DataVTEX, { hasVTEXToolbelt } from './data';
 
 let myStatusBarItem: vscode.StatusBarItem;
 let accountStatusBarItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
-	const { login, account, currentWorkspace, WORKSPACE } = VTEXData();
+  if (hasVTEXToolbelt()) {
+    const { login, account, currentWorkspace, WORKSPACE } = DataVTEX();
 
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
-	accountStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1001);
+    myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
+    accountStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1001);
 
-	myStatusBarItem.text = `$(git-merge) ${currentWorkspace}`;
-	accountStatusBarItem.text = `$(account) ${account}`;
+    myStatusBarItem.text = `$(git-merge) ${currentWorkspace}`;
+    accountStatusBarItem.text = `$(account) ${account}`;
 
-	myStatusBarItem.tooltip = `Workspace`;
-	accountStatusBarItem.tooltip = `${login}`;
+    myStatusBarItem.tooltip = `Workspace`;
+    accountStatusBarItem.tooltip = `${login}`;
 
-	myStatusBarItem.show();
-	accountStatusBarItem.show();
+    myStatusBarItem.show();
+    accountStatusBarItem.show();
 
-	fs.watchFile(WORKSPACE, () => {
-		const { login, account, currentWorkspace } = VTEXData();
-		myStatusBarItem.text = `$(git-merge) ${currentWorkspace}`;
-		myStatusBarItem.tooltip = `Workspace`;
+    fs.watchFile(WORKSPACE, () => {
+      const { login, account, currentWorkspace } = DataVTEX();
+      myStatusBarItem.text = `$(git-merge) ${currentWorkspace}`;
+      myStatusBarItem.tooltip = `Workspace`;
 
-		accountStatusBarItem.text = `$(account) ${account}`;
-		accountStatusBarItem.tooltip = `${login}`;
-	});
+      accountStatusBarItem.text = `$(account) ${account}`;
+      accountStatusBarItem.tooltip = `${login}`;
+    });
 
-	context.subscriptions.push(myStatusBarItem);
-	context.subscriptions.push(accountStatusBarItem);
+    context.subscriptions.push(myStatusBarItem);
+    context.subscriptions.push(accountStatusBarItem);
+  }
 }
