69fe54edbb103a3cba9e20ce365c8a95adc15136
Cláudio Borges <claudio.borges@corebiz.com.br>
2023-08-15 09:20:07 -0300

    fix(message): fix message to disconnected account

--- src/data.ts
+++ src/data.ts
@@ -1,28 +1,37 @@
-import * as fs from 'fs';
-import { join } from 'path';
-import { homedir } from 'os';
+import * as fs from "fs";
+import { join } from "path";
+import { homedir } from "os";
 
-const VTEX_FOLDER = join(homedir(), '.vtex');
-const SESSION_FOLDER = join(VTEX_FOLDER, 'session');
-const SESSION = join(SESSION_FOLDER, 'session.json');
-const WORKSPACE = join(SESSION_FOLDER, 'workspace.json');
+const VTEX_FOLDER = join(homedir(), ".vtex");
+const SESSION_FOLDER = join(VTEX_FOLDER, "session");
+const SESSION = join(SESSION_FOLDER, "session.json");
+const WORKSPACE = join(SESSION_FOLDER, "workspace.json");
 
 export default function DataVTEX() {
-    const { account, login } = JSON.parse(fs.readFileSync(SESSION, 'utf8'));
-    const { currentWorkspace } = JSON.parse(fs.readFileSync(WORKSPACE, 'utf8'));
+    const { account, login } = JSON.parse(fs.readFileSync(SESSION, "utf8"));
+    const { currentWorkspace } = JSON.parse(fs.readFileSync(WORKSPACE, "utf8"));
+
+    if (account === undefined) {
+        return {
+            login,
+            account: "Disconnected",
+            currentWorkspace: "Disconnected",
+            WORKSPACE,
+        };
+    }
 
     return {
         login,
         account,
         currentWorkspace,
-        WORKSPACE
+        WORKSPACE,
     };
 }
 
 export function hasVTEXToolbelt() {
-    return fs.existsSync(VTEX_FOLDER)
+    return fs.existsSync(VTEX_FOLDER);
 }
 
 export function hasVTEXSession() {
-    return fs.existsSync(SESSION_FOLDER)
+    return fs.existsSync(SESSION_FOLDER);
 }
